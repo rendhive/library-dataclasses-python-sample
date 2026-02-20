5780c4ead7a4b55b30a20175538927931ad71c8d
rendhive <rendhivecode@gmail.com>
2026-02-21 00:05:10 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul dataclasses.py

---
+++ 1. Mengimpor Modul dataclasses.py
@@ -0,0 +1,5 @@
+from dataclasses import dataclass
+
+print("Modul dataclasses berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk menggunakan dekorator dataclass.
+# Kondisi: Digunakan sebelum mendefinisikan kelas data menggunakan dataclass.
